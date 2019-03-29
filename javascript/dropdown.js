1d4875162365ffcf02726b8519b4268e606f0633
DarkRagingInferno <47670645+DarkRagingInferno@users.noreply.github.com>
2019-03-29 09:38:36 -0700

    Added some incomplete drop downs to the second page -- Raging

--- javascript/dropdown.js
+++ javascript/dropdown.js
@@ -1,6 +1,16 @@
 
     function dropDownFunction() {
         document.getElementById("myflowerdropdown").classList.toggle("show");
+        
+    }
+    function neardropDownFunction(){
+        document.getElementById("neardropdown").classList.toggle("show");
+    }
+    function near_bydropDownFunction(){
+        document.getElementById("near_bydropdown").classList.toggle("show");
+    }
+    function etcdropDownFunction(){
+        document.getElementById("etcdropdown").classList.toggle("show");
     }
 
     // Close dropdown 
